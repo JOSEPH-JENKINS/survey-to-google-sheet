da1b26d578f02d7b9542016e2917ff6225e7b60a
Joseph Jenkins <92091383+JOSEPH-JENKINS@users.noreply.github.com>
2021-12-09 01:27:50 +0100

    little minor update on this file

--- js/main.js
+++ js/main.js
@@ -10,12 +10,12 @@ INPUTS.forEach(el => el.addEventListener('click', e => updateValue(e)));
 let choice = null;
 let previousLink = null;
 
-function myFunction() {
+function myFunctionOne() {
     // Declare variables
     var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('myInput');
+    input = document.querySelector('.one');
     filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
+    ul = document.querySelector(".out-one");
     li = ul.getElementsByTagName('li');
   
     // Loop through all list items, and hide those who don't match the search query
@@ -23,7 +23,27 @@ function myFunction() {
       a = li[i].getElementsByTagName("a")[0];
       txtValue = a.textContent || a.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
+        li[i].style.display = "block";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
+
+  function myFunctionTwo() {
+    // Declare variables
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.querySelector('.two');
+    filter = input.value.toUpperCase();
+    ul = document.querySelector(".out-two");
+    li = ul.getElementsByTagName('li');
+  
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "block";
       } else {
         li[i].style.display = "none";
       }
@@ -36,11 +56,21 @@ document.querySelectorAll("#myUL a").forEach(link =>{
         if(previousLink){
             previousLink.classList.remove("live");
         }
+        e.target.parentNode.parentNode.previousElementSibling.value = link.innerHTML;
         link.classList.add("live");
         previousLink = link;
         choice = link.innerHTML;
     })
 });
+
+let message = `Thank you for taking the time to complete the survey.
+Your responses will be read by an administrator and used to make changes to our services.`;
+
+function resultSet(){
+  document.querySelector(".survey-emojis").style.display = "none";
+  document.querySelector(".survey-bottom").style.display = "none";
+  document.querySelector(".survey-head h1").innerHTML = message;
+}
 
 document.querySelector("#submit-all").addEventListener("click", (e)=>{
     e.preventDefault();
@@ -56,7 +86,7 @@ document.querySelector("#submit-all").addEventListener("click", (e)=>{
       method: "POST",
       body: submitedFormData,
     }).then(res => res.json())
-      .then(el => alert("Sent! Thanks"))
+      .then(el => resultSet())
 });
 
 window.addEventListener('load', ()=>{
